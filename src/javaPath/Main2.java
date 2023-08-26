e8cf584e9e5874504f2a37add22e89c931d6e275
davidbaba <davidbabasalifu2020@gmail.com>
2023-08-26 10:28:49 +0100

    Added main2

---
+++ src/javaPath/Main2.java
@@ -0,0 +1,29 @@
+package javaPath;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Integer n1 =21;
+        String name = "David";
+        int n2 =21;
+        long n = 2L;
+        boolean n3 = true;
+        double n4 = 2.34;
+        Long n5 =2L;
+        char m = 'w';
+
+
+        if(n2>23)
+        {
+            System.out.println("Yea, biggger");
+        }else
+        {
+            System.out.println("No, smaller");
+        }
+
+        for(int a=0; a<n2; a++) {
+            System.out.println(a);
+        }
+
+    }
+}
